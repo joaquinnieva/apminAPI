c2014c6803ee355e3ce86a18020a7c4c7a39990e
joaquinnieva <joacoo.nieva@gmail.com>
2022-06-23 20:49:34 -0300

    update credentials

--- src/database.js
+++ src/database.js
@@ -1,3 +1,3 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("postgresql://postgres:JaFUPTmlpu31IC7mKh8f@containers-us-west-35.railway.app:6613/railway");
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
