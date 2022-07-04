4b2583bb01ee6975ad115586c84d61a5a3e13d3d
joaquinnieva <joacoo.nieva@gmail.com>
2022-07-03 22:02:53 -0300

    add route to filter by author and fix edit route

--- src/routes/Shipping.routes.js
+++ src/routes/Shipping.routes.js
@@ -1,31 +1,33 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-// User Model
+// Ship Model
 import { Shippings } from '../models/Shipping.js';
 
 // GET Shipping by id
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
   try {
-    const user = await Shippings.findOne({
+    const ship = await Shippings.findOne({
       where: {
         id,
       },
     });
-    res.json({ name: user.name, data: user.data }).end();
+    res.json({ name: ship.name, data: ship.data }).end();
   } catch (error) {
     res.status(500).json({
       message: error.message,
     }).end();
   }
 });
-// GET all Users
-router.get('/', async (req, res, next) => {
+// GET all Ships
+router.get('/author/:author', async (req, res, next) => {
+  const { author } = req.params;
   try {
     const shippings = await Shippings.findAll({
-      atributes: ['id', 'author', 'title', 'data'],
+      where: {
+        author,
+      },
     });
     res.json(shippings).end();
   } catch (error) {
@@ -58,54 +60,41 @@ router.post('/', async (req, res, next) => {
 });
 
 // EDIT a Shipping by ID
-router.put('/:name', async (req, res, next) => {
-  const { name } = req.params;
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
   const { data } = req.body;
-  const authorization = req.get('authorization');
-  let token = null;
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    token = authorization.substring(7);
-  }
-  const decodedToken = jwt.verify(token,'token');
-  
-  if (decodedToken && (name == decodedToken.name)) {
     try {
-      const user = await Users.findOne({
+      const ship = await Shippings.findOne({
         where: {
-          name,
+          id,
         },
       });
 
-      user.data = data;
-      await user.save();
+      ship.data = data;
+      await ship.save();
       res.json({
-        message: 'User updated succesfully',
-        data: user.data,
+        message: 'Ship updated succesfully',
+        ship: ship,
       }).end();
     } catch (error) {
       return res.status(500).json({ message: error.message }).end();
     }
-  }else{
-    res.status(400).json({
-      message: 'User not updated succesfully, validate your credentials',
-    }).end();
-  }
 });
 
 // DELETE a Shipping by ID
 router.delete('/:id', async (req, res, next) => {
   const { id } = req.params;
-  const user = await Users.findByPk(id);
-  if (user) {
-    await Users.destroy({
+  const ship = await Shippings.findByPk(id);
+  if (ship) {
+    await Shippings.destroy({
       where: {
         id,
       },
     });
-    return res.sendStatus(204).json({ message: 'User deleted id:' + id }).end();
+    return res.sendStatus(204).json({ message: 'Shipping deleted id:' + id }).end();
   }
-  if (!user) {
-    return res.status(500).json({ message: 'User not found' }).end();
+  if (!ship) {
+    return res.status(500).json({ message: 'Shipping not found' }).end();
   }
 });
 
